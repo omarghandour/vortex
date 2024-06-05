bb34975e97f4d7b1de1b8d16931dcca47ba21b78
omarghandour <alex.forget50@gmail.com>
2024-06-05 04:41:14 +0300

    wtf is going on with the path XD

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
 import "./com.css";
+import logo from "../assets/logo.svg";
 const Home = () => {
   return (
-    <div className=" home h-full bg-center bg-no-repeat bg-cover flex flex-col items-start justify-between">
-      <img src="../assets/logo.svg" alt="" className=" h-48 ml-[10%]" />
+    <div className=" bg-[url('../src/assets/main.png')] h-full bg-center bg-no-repeat bg-cover flex flex-col items-start justify-between">
+      <img src={logo} alt="" className=" h-48 ml-[10%]" />
       <div className=" m-28 text-white sm:m-16 xs:m-8">
         <h1
           style={{ lineHeight: "1" }}
