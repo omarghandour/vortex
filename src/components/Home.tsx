66504a5262fa5a14816f8310c4df2376833ca383
omarghandour <alex.forget50@gmail.com>
2024-06-05 04:33:26 +0300

    logo path fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import "./com.css";
 const Home = () => {
   return (
     <div className=" home h-full bg-center bg-no-repeat bg-cover flex flex-col items-start justify-between">
-      <img src="../../src/assets/logo.svg" alt="" className=" h-48 ml-[10%]" />
+      <img src="../src/assets/logo.svg" alt="" className=" h-48 ml-[10%]" />
       <div className=" m-28 text-white sm:m-16 xs:m-8">
         <h1
           style={{ lineHeight: "1" }}
